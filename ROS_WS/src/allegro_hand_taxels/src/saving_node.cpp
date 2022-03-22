e525d56fea94b913a80e2ec79e4f7035456fbe77
Abed <abed-al-rahman.al-mrad.1@ens.etsmtl.ca>
2022-03-22 18:17:52 -0400

    Added Gazebo Simulation (1300 subscribers approach)

--- ROS_WS/src/allegro_hand_taxels/src/saving_node.cpp
+++ ROS_WS/src/allegro_hand_taxels/src/saving_node.cpp
@@ -7,13 +7,17 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "saving_commander");
   ros::NodeHandle nh;
   std_msgs::String msg;
+  //Publisher for hardware setup points saving
   ros::Publisher pub = nh.advertise<std_msgs::String>("saving_command", 1000);
+  //Publisher for simulation setup points saving
+  ros::Publisher pub_gazebo = nh.advertise<std_msgs::String>("saving_order", 1000);
   ros::Rate rate(10);
   while (ros::ok())
   {
     msg.data = "true";
     ROS_INFO("%s", msg.data.c_str());
     pub.publish(msg);
+    pub_gazebo.publish(msg);
     ros::spinOnce();
     rate.sleep();
   }
